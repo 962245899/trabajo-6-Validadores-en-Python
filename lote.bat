90d64419b0b7aa142d8a9bdea7669eaba3491b75
962245899 <57157985+962245899@users.noreply.github.com>
2019-11-17 14:40:19 -0500

    Add files via upload

---
+++ lote.bat
@@ -0,0 +1,122 @@
+python "condicional 01.py" "cesar" "trabajador1" 10 2.5
+python "condicional 01.py" "renzo" "trabajador1" 15 2.5
+python "condicional 01.py" "fernando" "trabajador1" 30 2.5
+
+python condiciona02.py "cesar" "trabajador2" 15 40
+python condiciona02.py "marcos" "trabajador2" 1 40
+python condiciona02.py "luis" "trabajador2" 48 40
+
+python condicional03.py "cesar" "trabajador03" 3 3 30 2500
+python condicional03.py "carlos" "trabajador03" 1 2 30 2500
+python condicional03.py "julio" "trabajador03"  2 1 30 2500
+
+python condicional04.py "cesar" "trabajador4" 2 1 1.7 0.8
+python condicional04.py "mario" "trabajador4" 3 2 1.7 0.8
+python condicional04.py "mercedes" "trabajador4" 5 0 1.7 0.8
+
+python condicional05.py "cesar" "trabajador5" 24 12 15 28
+python condicional05.py "susana" "trabajador5" 18 24 15 28
+python condicional05.py "maria" "trabajador5" 6 3 15 28
+
+
+
+
+
+python condicional07.py "cesar" "trabajador7" 2 4 1500 10
+python condicional07.py "henry" "trabajador7" 1 3 1500 10
+python condicional07.py "guiliana" "trabajador7" 0 10 1500 10
+
+
+python condicional08.py "cesar" "trabajador8" 3 2 3500 8000
+python condicional08.py "helver" "trabajador8" 1 1 3500 8000
+python condicional08.py "jesus" "trabajador8" 0 5 3500 8000
+
+python condiconal09.py "cesar" "trabajador9" 36 100 0.80 0.20
+python condiconal09.py "mercedes" "trabajador9" 24 150 0.80 0.20
+python condiconal09.py "mariana" "trabajador9" 6 100 0.80 0.20
+
+python condicional10.py "cesar" "trabajador10" 24 10 15 2
+python condicional10.py "cinthia" "trabajador10" 24 10 15 2
+python condicional10.py "lorena" "trabajador10" 24 10 15 2
+
+python condicional_doble01.py "cesar" "trabajador11" 15 10.90
+python condicional_doble01.py "elena" "trabajador11" 20 10.90
+python condicional_doble01.py "ana" "trabajador11" 18 10.90
+
+python condicional_doble02.py "cesar" "trabajador12" 20 18
+python condicional_doble02.py "maria" "trabajador12" 1 18
+python condicional_doble02.py "karen" "trabajador12" 12 18
+
+python condicional_doble03.py "cesar" "trabajador13" 3 3 30 2500
+python condicional_doble03.py "atara" "trabajador13" 2 1 30 2500
+python condicional_doble03.py "ever" "trabajador13" 0 5 30 2500
+
+python condicional_doble04.py "cesar" "trabajador14" 2 2 1.80 0.70
+python condicional_doble04.py "sheyla" "trabajador14" 3 0 1.80 0.70
+python condicional_doble04.py "nicol" "trabajador14" 1 5 1.80 0.70
+
+python condicional_doble05.py "cesar" "trabajador15" 12 6 15 28
+python condicional_doble05.py "nelida" "trabajador15" 6 18 15 28
+python condicional_doble05.py "margarita" "trabajador15" 18 1 15 28
+
+python condicional_doble06.py "cesar" "trabajador16" 12 12 2.50 1.20
+python condicional_doble06.py "carmen" "trabajador16" 6 12 2.50 1.20
+python condicional_doble06.py "mauricio" "trabajador16" 24 0 2.50 1.20
+
+python condicional_doble07.py "cesar" "trabajador17" 2 2 1800 10
+python condicional_doble07.py "daniel" "trabajador17" 1 3 1800 10
+python condicional_doble07.py "jose" "trabajador17" 0 5 1800 10
+
+python condicional_doble08.py "cesar" "trabajador18" 6 3 3500 8000
+python condicional_doble08.py "milagros" "trabajador18" 4 5 2500 8000
+python condicional_doble08.py "marta" "trabajador18" 1 2 2500 8000
+
+python condicional_doble09.py "cesar" "trabajador19" 36 12 0.80 0.20
+python condicional_doble09.py "rosa" "trabajador19" 24 6 0.80 0.20
+python condicional_doble09.py "samanta" "trabajador19" 48 36 0.80 0.20
+
+python condicional_doble10.py "cesar" "trabajador20" 36 12 15 2
+python condicional_doble10.py "pepe" "trabajador20" 48 36 15 2
+python condicional_doble10.py "juan" "trabajador20" 12 48 15 2
+
+python condicional_multiple01.py "cesar" "trabajador21" 15 9.50
+python condicional_multiple01.py "deyvid" "trabajador21" 14 9.50
+python condicional_multiple01.py "raquel" "trabajador21" 8 9.50
+
+python condicional_multiple02.py "cesar" "trabajador22" 15 9.50
+python condicional_multiple02.py "jimy" "trabajador22" 15 9.50
+python condicional_multiple02.py "julio" "trabajador22" 15 9.50
+
+python condicional_multiple03.py "cesar" "trabajador23" 3 2 30 2500
+python condicional_multiple03.py "yanela" "trabajador23" 4 1 30 2500
+python condicional_multiple03.py "keyla" "trabajador23" 1 0 30 2500
+
+python condicional_multiple04.py "cesar" "trabajador24" 2 4 1.80 0.90
+python condicional_multiple04.py "mario" "trabajador24" 5 2 1.80 0.90
+python condicional_multiple04.py "jose" "trabajador24"  1 3 1.80 0.90
+
+python condicional_multiple05.py "cesar" "trabajador25" 18 6 15 28
+python condicional_multiple05.py "adrian" "trabajador25" 6 1 15 28
+python condicional_multiple05.py "nanci" "trabajador25" 24 3 15 28
+
+python condicional_multiple06.py "cesar" "trabajador26" 24 6 2 1.20
+python condicional_multiple06.py "judy" "trabajador26" 15 9 2 1.20
+python condicional_multiple06.py "piero" "trabajador26" 6 36 2 1.20
+
+python condicional_multiple07.py "cesar" "trabajador27" 2 4 1800 10
+python condicional_multiple07.py "augusto" "trabajador27" 1 3 1800 10
+python condicional_multiple07.py "fernando" "trabajador27" 0 15 1800 10
+
+python condicional_multiple08.py "cesar" "trabajador28" 1 1 3500 8000
+python condicional_multiple08.py "mateo" "trabajador28" 0 2 3500 8000
+python condicional_multiple08.py "wilson" "trabajador28" 3 1 3500 8000
+
+python condicional_multiple09.py "cesar" "trabajador29" 24 100 0.80 0.20
+python condicional_multiple09.py "miguel" "trabajador29" 12 200 0.80 0.20
+python condicional_multiple09.py "walter" "trabajador29" 48 50 0.80 0.20
+
+python condicional_multiple10.py "cesar" "trabajador30" 24 12 15 2
+python condicional_multiple10.py "azucena" "trabajador30" 36 6 15 2
+python condicional_multiple10.py "damian" "trabajador30" 6 15 15 2
+
+#Cesar Maira Paz-192028G
